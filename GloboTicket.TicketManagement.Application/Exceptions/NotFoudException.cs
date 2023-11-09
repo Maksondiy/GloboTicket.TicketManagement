a9e76b987ee85c13fface6b5341f5051f9d27382
Pravda Maksym <maksoravda@icloud.com>
2023-11-09 23:44:55 +0200

    Add project files.

---
+++ GloboTicket.TicketManagement.Application/Exceptions/NotFoudException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Exeptions
+{
+    public class NotFoudException : Exception
+    {
+        public NotFoudException(string name, object key)
+            : base($"{name}({key}) is not found")
+        { 
+        }
+    }
+}
